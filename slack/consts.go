3c0e98a8991ec86323c99ce2173a5dac7abcf02c
tukejonny <ne250143@yahoo.co.jp>
2017-10-27 11:08:34 +0900

    fix bug :bug:

--- slack/consts.go
+++ slack/consts.go
@@ -2,7 +2,7 @@ package slack
 
 const (
 	SLACK_USERNAME = "柊 真冬"
-	SLACK_ICON_URL = "https://pbs.twimg.com/media/C9I4H5rUwAEmGHR.jp"
+	SLACK_ICON_URL = "https://pbs.twimg.com/media/C9I4H5rUwAEmGHR.jpg"
 	SLACK_CHANNEL  = "#isucon"
 
 	FAIL_COLOR    = "#e84949"
